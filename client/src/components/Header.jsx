b77856e4a7e2c8c44fd03a55d88beb1d03a5c501
stgHellis <etienneake@hotmail.fr>
2023-12-14 11:50:18 +0100

    Modification du Port du server Vite:5170

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { FaSearch } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+//import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 
 export default function Header() {
