0e9cf2d7cf6655e524d5d6d2ba32c7f42361365d
stgHellis <etienneake@hotmail.fr>
2023-12-17 16:12:00 +0100

    Création de Middleware et une fonction pour gérer les erreurs possibles

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,16 @@
+import { errorHandler } from "../Utils/error.js";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password: hashedPassword});
     try {
-        await newUser.save();
-        res.status(201).json("User created Successfully");    // Cela peut prendre beaucoup de temps pour la sauvegarde. C'est pourquoi on ajoute await devant newUser.
+        await newUser.save();       // Cela peut prendre beaucoup de temps pour la sauvegarde. C'est pourquoi on ajoute await devant newUser.
+        res.status(201).json("User created Successfully");    
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
             
 }
