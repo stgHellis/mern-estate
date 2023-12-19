f36b556682792fcd8296bbedcf3df7ffa123d146
stgHellis <etienneake@hotmail.fr>
2023-12-19 22:12:29 +0100

    Complete functionality of Image Upload, User Update and Delete User

--- api/index.js
+++ api/index.js
@@ -3,12 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js"
-
-export const test = (req, res) => {
-  res.json({
-    message: 'Api route is working!',
-  });
-};
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -21,6 +16,8 @@ mongoose.connect(process.env.MONGO).then( () => {
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
